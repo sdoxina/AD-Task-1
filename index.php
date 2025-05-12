5356274c0726b915d6d53e41acd4c130fdae1d32
sdoxina <202311026@fit.edu.ph>
2025-05-13 05:36:02 +0800

    feat(landing): implement footer functionality and styling

--- index.php
+++ index.php
@@ -44,7 +44,32 @@
         <p>Inside the wavy box!</p>
     </div>
     </section>
-    
+
+    <!-- Footer -->
+    <footer class="footer">
+        <div class="container">
+           <footer class="py-5">
+              <div class="row">
+                 <div class="col-6 col-md-2 mb-3">
+                    <ul class="footer flex-column list-unstyled">
+                       <li class="footer-item mb-2"><a href="./pages/page1/index.html" class="footer-link p-0 text-body-secondary">Home</a></li>
+                       <li class="footer-item mb-2"><a href="" class="footer-link p-0 text-body-secondary">Items</a></li>
+                    </ul>
+                 </div>
+                 <div class="col-6 col-md-2 mb-3">
+                    <h5>Web Designer</h5>
+                    <h6>Shane D. Oxina</h6>
+                 </div>
+                 <div class="col-md-7 offset-md-1 mb-3">
+                    <img src="./assets/img/" alt="Buttoncone Logo">
+                 </div>
+              </div>
+              <div class="d-flex flex-column flex-sm-row justify-content-between py-4 my-4 border-top">
+                 <p>Copyright &copy; 2025 buttoncone. All rights reserved.</p>
+              </div>
+           </footer>
+        </div>
+    </footer>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
