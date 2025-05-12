bfecdab2d5a14a8d72c9022a25808b823a9a9731
sdoxina <202311026@fit.edu.ph>
2025-05-13 05:34:01 +0800

    refactor(landing): wrap scallop box in a section for better semantic structure

--- index.php
+++ index.php
@@ -39,10 +39,12 @@
            </button>
         </div>
      </nav>
-
-    <div class="scallop-box">
+    <section>
+        <div class="scallop-box">
         <p>Inside the wavy box!</p>
     </div>
+    </section>
+    
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
