82019acbdb4c3cc0fd9e229349e3d2e6269cd841
sdoxina <202311026@fit.edu.ph>
2025-05-13 22:51:49 +0800

    feat(items): implement responsive navigation bar with logo and offcanvas menu

--- pages/items/index.php
+++ pages/items/index.php
@@ -10,7 +10,37 @@
     <link rel="icon" type="image/x-icon" href="./assets/img/buttonconeFav.png">
 </head>
 <body>
-    
+    <!-- Navigation Bar -->
+      <nav class="navbar navbar-expand-lg fixed-top">
+         <div class="container-fluid">
+            <a class="navbar-brand me-auto d-flex align-items-center" href="../../index.php">
+            <img src="./assets/img/buttonconeLogo.png" alt="Logo" class="navbar-img">
+            </a>
+            <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
+               <div class="offcanvas-header">
+                  <h5 class="offcanvas-title d-flex align-items-center" id="offcanvasNavbarLabel">
+                     <img src="./assets/img/buttonconeLogo.png" alt="Buttoncone Logo">
+                  </h5>
+                  <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+               </div>
+               <div class="offcanvas-body">
+                  <ul class="navbar-nav ms-auto pe-3">
+                     <li class="nav-item">
+                        <a class="nav-link mx-lg-2" aria-current="page" href="../../index.php">Home</a>
+                     </li>
+                     <li class="nav-item">
+                        <a class="nav-link active mx-lg-2" aria-current="page" href="index.php">Items</a>
+                     </li>
+                  </ul>
+               </div>
+            </div>
+            <button class="navbar-toggler pe-0" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+            </button>
+         </div>
+      </nav>
+
+
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
       <script src="./assets/js/script.js"></script>
 </body>
